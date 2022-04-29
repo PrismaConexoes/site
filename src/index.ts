ee8d0a051f466be1c62acd1e1fc9e44f20e3c056
EduardoProto <silvaproto@yahoo.com.br>
2022-04-29 11:34:32 -0300

    doze commit

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,8 @@
 import { AppDataSource } from "./data-source"
 import { UserController } from "./controller/UserController"
 import { Request, Response } from "express"
-import { Userr } from "./entity/Userr"
-import { json } from "stream/consumers"
 
 AppDataSource.initialize().then(async () => {
-
-    const usrCtrl = require('./controller/UserController')
 
     //Configuração do express app
     const express = require('express')
@@ -21,6 +17,11 @@ AppDataSource.initialize().then(async () => {
     const bp = require('body-parser')
     app.use(bp.json())
     app.use(bp.urlencoded({extended: true}))
+    
+    //Configuração do reCaptcha
+    const captcha = require('express-recaptcha').RecaptchaV3
+    const options = { hl: 'pt' }
+    const recaptcha = new captcha('6LciB7AfAAAAAMKT3Nlr-Ch2oCIWetsL58dMkCUC', '6LciB7AfAAAAAP2Z5z2iGzsk3nug44E3sJFjwRvC', options)
 
     //Engine express-handlebars
     const exphbs  = require('express-handlebars');
@@ -75,9 +76,9 @@ AppDataSource.initialize().then(async () => {
     })
 
     //Rota Login
-    app.get('/login',  (req: Request, res: Response , next: Function ) => {
-        
-        res.render("login.hbs")
+    app.get('/login', recaptcha.middleware.render,  (req: Request, res: Response , next: Function ) => {
+
+        res.render("login.hbs", { captcha: res.recaptcha.render() })
         
     })
     
@@ -108,7 +109,7 @@ AppDataSource.initialize().then(async () => {
         const result = controler.one(req, res, next);
 
         if(result instanceof Promise){
-            result.then(result => result !== null && result !== undefined ? res.send(result): undefined);
+            result.then(result => result !== null && result !== undefined ? res.send(result): res.send("Usuário não encontrado!"));
         }else if(result !== null && result !== undefined){
             res.json(result);
         }
