1f94a7f7cbd841b303cc4f598a19c0ecbed8d179
EduardoProto <silvaproto@yahoo.com.br>
2022-05-03 20:26:33 -0300

    quinze commit

--- src/data-source.ts
+++ src/data-source.ts
@@ -18,21 +18,4 @@ export const AppDataSource = new DataSource({
     migrations: [],
     subscribers: [],
 })
-<<<<<<< HEAD
-//Configuração testada - conexão estabelecida com sucesso. (Ver política de troca de password)
-/**export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "motty.db.elephantsql.com",
-    port: 5432,
-    username: "fuibglyn",
-    password: "j2qZThUzxPhA-oMe4IDxTYIk9JYBzSl1",
-    database: "fuibglyn",
-    synchronize: true,
-    logging: false,
-    entities: [Userr, Session],
-    migrations: [],
-    subscribers: [],
-})*/
-=======
 
->>>>>>> 834781689f116d447a7c9590b30dea42c810152b
