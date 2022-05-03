b65b14d7fea436964ac59d2de272d036a026cfe5
silvaproto <57446445+silvaproto@users.noreply.github.com>
2022-05-03 19:03:31 -0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,11 +7,11 @@ import { Session } from "./entity/Session"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "ec2-3-229-252-6.compute-1.amazonaws.com",
+    host: "motty.db.elephantsql.com (motty-01)",
     port: 5432,
-    username: "dmipgolrmbdpvf",
-    password: "7a5e3c1a09f37d1e14e458a98ed4b54b9092c50c808b6fc178ae934b26625640",
-    database: "d8hdk425hc1sd0",
+    username: "fuibglyn",
+    password: "j2qZThUzxPhA-oMe4IDxTYIk9JYBzSl1",
+    database: "fuibglyn",
     synchronize: true,
     logging: false,
     entities: [Userr, Session],
