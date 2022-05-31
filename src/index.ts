905edfdaf89aa72f6d7984d4dc6fe8816ad7d2f4
EduardoProto <silvaproto@yahoo.com.br>
2022-05-30 23:26:02 -0300

    vinteenove commit

--- src/index.ts
+++ src/index.ts
@@ -276,7 +276,7 @@ AppDataSource.initialize().then(async () => {
     })
 
         //Rota complementat para validação de conta
-        app.get('/validarSecret',  (req: any, res: any , next: NextFunction ) => {
+        app.post('/validarSecret',  (req: any, res: any , next: NextFunction ) => {
             recaptcha.verify(req, function (error, data) {
                 if (!error) {
                     let senha = req.body.password
