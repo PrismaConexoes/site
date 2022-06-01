aa5822b74d7ae471f755b033491a2245db11b881
EduardoProto <silvaproto@yahoo.com.br>
2022-06-01 15:49:57 -0300

    trinta commit

--- src/email.ts
+++ src/email.ts
@@ -5,7 +5,7 @@ export default function email(link){
                     '<div style="margin-top: 5%; color: white">Prezado(a) Cliente,<div>'+
                     '<div style="color: white">recebemos o seu pedido de cadastro em nossa plataforma!<div>'+
                     '<div style="color: white">Para concluir seu cadastro, pedimos que clique no botão e siga as instruções<div>'+
-                    '<div style="margin-top: 50px"><a href="'+link+'" style="background-color: green; color: black; margin-left: 15px; margin-right: 15px; height: 30px;">Validar Cadastro</a></div>'+
+                    '<div style="margin-top: 50px; height: 30px; width: 70px; "><a href="'+link+'" style="background-color: blue; color: white; height: 100%; width: 100%;">Validar Cadastro</a></div>'+
                     '<div style="margin-top: 50px;">Saudações da Prisma Conexão!<div>'+
                     '<div style="margin-top: 50px;"><spam>Caso tenha recebido esta mensagem por engano, por favor desconsidere.</spam></div>'+
                     '</div>'
