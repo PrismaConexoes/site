fea735f448b1a1500cf23a77ed45c1ecaa373cff
EduardoProto <silvaproto@yahoo.com.br>
2022-06-01 15:42:05 -0300

    trinta commit

--- src/email.ts
+++ src/email.ts
@@ -3,11 +3,11 @@ export default function email(link){
 
     let mensagem = '<div style="background-color: gray;"}>'+
                     '<div style="margin-top: 5%; color: white">Prezado(a) Cliente,<div>'+
-                    '<div color: white">recebemos o seu pedido de cadastro em nossa plataforma!<div>'+
-                    '<div color: white">Para concluir seu cadastro, pedimos que clique no botão e siga as instruções<div>'+
-                    '<div style="background-color: green; margin-top: 5%"><a href="'+link+'" style=" color: black; margin-left: 5%; margin-right: 5%; height: 30px;">Validar Cadastro</a><div>'+
-                    '<div style="margin-top: 5%;">Saudações da Prisma Conexão!<div>'+
-                    '<div style="margin-top: 5%;"><spam>Caso tenha recebido esta mensagem por engano, por favor desconsidere.</spam></div>'+
+                    '<div style="color: white">recebemos o seu pedido de cadastro em nossa plataforma!<div>'+
+                    '<div style="color: white">Para concluir seu cadastro, pedimos que clique no botão e siga as instruções<div>'+
+                    '<div style="margin-top: 50px"><a href="'+link+'" style="background-color: green; color: black; margin-left: 15px; margin-right: 15px; height: 30px;">Validar Cadastro</a></div>'+
+                    '<div style="margin-top: 50px;">Saudações da Prisma Conexão!<div>'+
+                    '<div style="margin-top: 50px;"><spam>Caso tenha recebido esta mensagem por engano, por favor desconsidere.</spam></div>'+
                     '</div>'
 
     return mensagem
