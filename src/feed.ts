8fc0d638b4cbff2f517c85a7c3d10e69aebf5c72
EduardoProto <silvaproto@yahoo.com.br>
2023-01-13 09:32:37 -0300

    v1

--- src/feed.ts
+++ src/feed.ts
@@ -2,15 +2,37 @@ export default async function getFeed(){
 
     let Parser = require('rss-parser');
     let parser = new Parser();
-
-    let feed = await parser.parseURL('https://adrenaline.com.br/rss');
     
+    let feed = await parser.parseURL('https://res.stj.jus.br/hrestp-c-portalp/RSS.xml');
+    //let feed = await parser.parseURL('https://adrenaline.com.br/rss');
     let jfeed = JSON.parse(JSON.stringify(feed.items));
 
-    let rss = { 
-        img0: jfeed[0].enclosure.url, 
-        img1: jfeed[1].enclosure.url, 
-        img2: jfeed[2].enclosure.url,
+    let rss = {
+  
+        img0: () => {
+            if(jfeed[0].hasOwnProperty("enclosure")){
+                if(jfeed[0].enclosure.hasOwnProperty("url")){
+                    return jfeed[0].enclosure.url
+                }
+            }
+            else {return null}
+        }, 
+        img1:() => {
+            if(jfeed[1].hasOwnProperty("enclosure")){
+                if(jfeed[1].enclosure.hasOwnProperty("url")){
+                    return jfeed[1].enclosure.url
+                }
+            }
+            else {return null}
+        }, 
+        img2:() => {
+            if(jfeed[2].hasOwnProperty("enclosure")){
+                if(jfeed[2].enclosure.hasOwnProperty("url")){
+                    return jfeed[2].enclosure.url
+                }
+            }
+            else {return null}
+        },
         title0: jfeed[0].title ,
         title1: jfeed[1].title ,
         title2: jfeed[2].title ,
@@ -19,7 +41,31 @@ export default async function getFeed(){
         pubData2: jfeed[2].pubDate,
         link0: jfeed[0].link,
         link1: jfeed[1].link,
-        link2: jfeed[2].link
+        link2: jfeed[2].link,
+        pres0:() => {
+            if(jfeed[0].hasOwnProperty("enclosure")){
+                if(jfeed[0].enclosure.hasOwnProperty("url")){
+                    return true
+                }
+            }
+            else {return false}
+        },
+        pres1:() => {
+            if(jfeed[1].hasOwnProperty("enclosure")){
+                if(jfeed[1].enclosure.hasOwnProperty("url")){
+                    return true
+                }
+            }
+            else {return false}
+        },
+        pres2:() => {
+            if(jfeed[2].hasOwnProperty("enclosure")){
+                if(jfeed[2].enclosure.hasOwnProperty("url")){
+                    return true
+                }
+            }
+            else {return false}
+        } 
     }
 
     return rss;
