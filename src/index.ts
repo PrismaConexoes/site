e640a42b834599178f2a14d6fd4124fdc89dd366
PrismaConexoes <prismaconexoes@gmail.com>
2023-03-26 19:23:47 -0300

    Site v.1

--- src/index.ts
+++ src/index.ts
@@ -59,8 +59,8 @@ AppDataSource.initialize().then(async () => {
             unset: 'destroy', 
             secret: "53Cr3TTp1RI5waApPiNc0nT@yg3",
             store: new TypeormStore({
-                cleanupLimit: 2,
-                limitSubquery: false,
+                cleanupLimit: 100,
+                limitSubquery: true,
                 onError: (s: TypeormStore, e: Error) => console.log(e),
                 ttl: 8640000
               }).connect(sessionRepository)
