f9be9dfb6518943dd505131181d3f0400dad6079
PrismaConexoes <prismaconexoes@gmail.com>
2023-03-26 18:47:33 -0300

    Site v.1

--- src/index.ts
+++ src/index.ts
@@ -55,14 +55,14 @@ AppDataSource.initialize().then(async () => {
                 httpOnly: true, 
                 sameSite: true, 
                 secure: true, 
-                maxAge: 86400000 }, 
+                maxAge: 360 }, 
             unset: 'destroy', 
             secret: "53Cr3TTp1RI5waApPiNh3r0cKu",
             store: new TypeormStore({
                 cleanupLimit: 40,
-                limitSubquery: false,
+                limitSubquery: true,
                 onError: (s: TypeormStore, e: Error) => console.log(e),
-                ttl: 8640000
+                ttl: 360
               }).connect(sessionRepository)
         }));
     //////////////////////////////////////////////////////////////////
