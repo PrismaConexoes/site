8619419b73ad97c60290891f2aee84fb4368a28b
PrismaConexoes <prismaconexoes@gmail.com>
2023-03-26 17:32:27 -0300

    Site v.1

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,8 @@ AppDataSource.initialize().then(async () => {
                 httpOnly: true, 
                 sameSite: true, 
                 secure: true , 
-                maxAge: 86400000 }, 
+                expires:  86400000,
+             }, 
             unset: 'destroy', 
             secret: "53Cr3TTp1RI5waApPiNh3r0cKu",
             store: new TypeormStore({
