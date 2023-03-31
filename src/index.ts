5c74dc695bceafed568768c23da418f31772f8ee
PrismaConexoes <prismaconexoes@gmail.com>
2023-03-31 07:45:17 -0300

    Site v.1

--- src/index.ts
+++ src/index.ts
@@ -472,7 +472,6 @@ AppDataSource.initialize().then(async () => {
                     })
 
             }).then(() =>{
-                let admins = JSON.stringify(adms_arr)
                 res.render('administradores.hbs', {data : adms_arr})
             })
             
